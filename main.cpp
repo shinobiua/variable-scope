e40bdf3b9e8f4311e5dd06b24f4931c202ee6d2a
Ivan Yekimov <lalavawe@gmail.com>
2018-07-27 00:34:23 +0300

    Add files via upload
    Source code and an executable to check if the output of int d is exactly the sum of int a (global int) and c (local int)

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+  int a = 5;
+
+    int main() {
+
+         int b = 100, c = 15;
+
+         int d = a + c;
+
+         cout << d;
+
+         return 0; 
+
+}
